dc5a7cc8a8f4b2e5f1d77fb17f037893cbdafd11
Stefan Jankovic <stefanjankovic31@gmail.com>
2019-03-13 08:59:10 +0100

    Changes v6

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ var currentRight = 1;
 
 var animationInterval = 2000;
 
-document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
-
 $( document ).ready(function() {
 
 	$('html, body').css({
